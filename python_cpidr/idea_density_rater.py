b07d9ebd240e0b7f78cc144df4371d181b1fdb6b
Jason Robison <jrrobison1@hoodsen.me>
2024-08-11 20:25:04 -0600

    Add temporary debug print statement

--- python_cpidr/idea_density_rater.py
+++ python_cpidr/idea_density_rater.py
@@ -3,9 +3,10 @@ from python_cpidr.idea_density_rater_rules import apply_idea_counting_rules
 from python_cpidr.tagger import tag_text
 from python_cpidr.word_item import WordList
 
+
 def rate_text(text: str, speech_mode: bool = False) -> WordList:
     t = tag_text(text)
-    
+
     word_list = WordList(t)
     apply_idea_counting_rules(word_list.items, speech_mode)
 
@@ -18,5 +19,12 @@ def rate_text(text: str, speech_mode: bool = False) -> WordList:
             pc += 1
 
     density = pc / wc
-    
-    return density, word_list
+
+    print(f"Word count: {wc}, prop count: {pc}, density: {density}")
+    print(f"Word list:")
+    for word in word_list.items:
+        print(
+            f"Token: [{word.token}], tag: [{word.tag}], is_word: [{word.is_word}], is_prop: [{word.is_prop}], rule_number: [{word.rule_number}]"
+        )
+
+    return wc, pc, density, word_list

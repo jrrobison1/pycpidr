d3cf649032138a111e1512a6cf4611d7bb605889
Jason Robison <jrrobison1@hoodsen.me>
2024-08-11 20:25:43 -0600

    Update main to use new rate_text function signature

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import sys
 from python_cpidr.idea_density_rater import rate_text
 
 
-
 def main(text):
-    density, word_list = rate_text(text, speech_mode=False)
+    _, _, density, word_list = rate_text(text, speech_mode=False)
 
     print(f"Density: {density}")
     print(f"Word list:")
     for word in word_list.items:
-        print(f"Token: {word.token}, tag: {word.tag}, is_word: {word.is_word}, is_prop: {word.is_prop}, rule_number: {word.rule_number}")
-    
+        print(
+            f"Token: [{word.token}], tag: [{word.tag}], is_word: [{word.is_word}], is_prop: [{word.is_prop}], rule_number: [{word.rule_number}]"
+        )
 
 
 if __name__ == "__main__":

c962527ef979ed43733f97f4f7eb116f1832a843
Jason Robison <jrrobison1@hoodsen.me>
2024-08-13 09:12:50 -0600

    Add docstrings

--- pycpidr/tagger.py
+++ pycpidr/tagger.py
@@ -4,16 +4,34 @@ _nlp = None
 
 
 def get_nlp():
+    """
+    Load and return the spaCy English language model.
+
+    Returns:
+        spacy.lang.en.English: The loaded spaCy English language model.
+    """
     global _nlp
     if _nlp is None:
         _nlp = spacy.load("en_core_web_sm")
     return _nlp
 
 
-def tag_text(text, nlp=get_nlp()):
-    _nlp = nlp
+def tag_text(text, nlp=None):
+    """
+    Perform part-of-speech tagging on the input text.
 
-    doc = _nlp(text)
+    Args:
+        text (str): The input text to be tagged.
+        nlp (spacy.lang.en.English, optional): A pre-loaded spaCy model. If None,
+            the default model will be loaded.
+
+    Returns:
+        list of tuple: A list of (token, tag) pairs for each token in the input text.
+    """
+    if nlp is None:
+        nlp = get_nlp()
+
+    doc = nlp(text)
     tagged_tokens = [(token.text, token.tag_) for token in doc]
 
     return tagged_tokens
--- pycpidr/word_item.py
+++ pycpidr/word_item.py
@@ -2,21 +2,61 @@ from typing import List, Tuple
 
 
 class WordListItem:
-    def __init__(self, token: str = "", tag: str = "", is_word: bool = False, is_prop: bool = False, rule_number: int = 0):
+    """
+    Represents a single word or token in a text, along with its properties.
+
+    Attributes:
+        token (str): The actual word or token.
+        tag (str): The part-of-speech tag for the token.
+        is_word (bool): Indicates if the token is considered a word.
+        is_prop (bool): Indicates if the token is considered a proposition.
+        rule_number (int): The number of the rule that was applied to this token in determining if is a proposition.
+    """
+
+    def __init__(
+        self,
+        token: str = "",
+        tag: str = "",
+        is_word: bool = False,
+        is_prop: bool = False,
+        rule_number: int = 0,
+    ):
         self.token = token
         self.tag = tag
         self.is_word = is_word
         self.is_prop = is_prop
         self.rule_number = rule_number
 
+
 class WordList:
+    """
+    Represents a list of WordListItems, typically corresponding to a full text.
+
+    This class is responsible for creating and storing WordListItems based on
+    tagged text input.
+
+    Attributes:
+        items (List[WordListItem]): The list of WordListItems.
+    """
+
     def __init__(self, tagged_text: List[Tuple[str, str]]):
+        """
+        Initializes a WordList from tagged text.
+
+        Args:
+            tagged_text (List[Tuple[str, str]]): A list of (word, tag) tuples.
+
+        If tagged_text is empty, initializes with 10 empty WordListItems.
+        Otherwise, creates a WordListItem for each word in the tagged text.
+        """
         self.items = []
         word_list_item = WordListItem("", "", False, False, 0)
-        
+
         if not tagged_text:
             for i in range(10):
                 self.items.append(word_list_item)
-        
+
         for tagged_word in tagged_text:
-            self.items.append(WordListItem(tagged_word[0], tagged_word[1], False, False, 0))
+            self.items.append(
+                WordListItem(tagged_word[0], tagged_word[1], False, False, 0)
+            )
